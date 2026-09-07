f2774b44594ef8e02dd8e4fb43b9ecd526c7c328
aykutgumus60-maker <aykutgumus60@gmail.com>
2026-09-07 08:38:35 +0300

    Change MAJORITY_THRESHOLD default value to 17

--- bot.py
+++ bot.py
@@ -42,7 +42,7 @@ CRSI_THRESHOLD = float(os.environ.get("CRSI_THRESHOLD", 18))
 UO_THRESHOLD = float(os.environ.get("UO_THRESHOLD", 36))
 RVI_THRESHOLD = float(os.environ.get("RVI_THRESHOLD", 20))
 ROC_THRESHOLD = float(os.environ.get("ROC_THRESHOLD", -55))
-MAJORITY_THRESHOLD = float(os.environ.get("MAJORITY_THRESHOLD", 36))
+MAJORITY_THRESHOLD = float(os.environ.get("MAJORITY_THRESHOLD", 17))
 MFI_THRESHOLD = float(os.environ.get("MFI_THRESHOLD", 24))
 
 TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN", "")
